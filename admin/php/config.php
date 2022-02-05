23eb4d6206d66d3d1a6daf12af0601074374a523
Vince John Perez <perezvj.social@gmail.com>
2022-02-05 22:42:53 +0800

    Removed commented lines

--- admin/php/config.php
+++ admin/php/config.php
@@ -1,10 +1,7 @@
 <?php
-    //user: debian-sys-maint
-    // password: l6pdxQO6O3wIFWZs
-
     $servername = "localhost";
-    $username = "debian-sys-maint";
-    $password = "l6pdxQO6O3wIFWZs";
+    $username = "root";
+    $password = "";
     $dbname = "fastech";
 
     // crearte connection
@@ -12,7 +9,7 @@
 
     // check connection
     if($con->connect_error) {
-    die("Connection Failed : " . $connect->error);
+        die("Connection Failed : " . $connect->error);
     } else {
     // echo "Successfully Connected";	
     }
